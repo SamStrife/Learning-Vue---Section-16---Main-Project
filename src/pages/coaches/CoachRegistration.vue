4f9523061deb126ca09b98c5743de4bfcf661ca4
Sam Fletcher <sam.a.fletcher@gmail.com>
2022-01-21 06:54:36 +0000

    Added registration form for coach and linked it to register view.

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,15 @@
-<template>Register</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register As A Coach Now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: { CoachForm },
+};
+</script>
