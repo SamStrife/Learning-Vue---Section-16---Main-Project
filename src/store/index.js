23db8f725cff2580a5c874474f800bce097e5204
Sam <sam.a.fletcher@gmail.com>
2022-01-22 12:51:50 +0000

    Built the contact pages and linked to Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
+import RequestsModule from './modules/requests/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
+    requests: RequestsModule,
   },
   state() {
     return {
