677eb632beb046f5d2c331366d74a478f94b0662
Sam <sam.a.fletcher@gmail.com>
2022-01-23 10:53:27 +0000

    Added base dialog and error handling to coach loading

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,7 @@
 <template>
+  <base-dialog :show="!!error" title="An error occured" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <coach-filter @change-filter="setFilters"></coach-filter>
   </section>
@@ -41,6 +44,7 @@ export default {
         career: true,
       },
       isLoading: false,
+      error: null,
     };
   },
   components: { CoachItem, CoachFilter },
@@ -73,8 +77,15 @@ export default {
     },
     async loadCoaches() {
       this.isLoading = true;
-      await this.$store.dispatch('coaches/loadCoaches');
+      try {
+        await this.$store.dispatch('coaches/loadCoaches');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
       this, (this.isLoading = false);
+    },
+    handleError() {
+      this.error = null;
     },
   },
   created() {
