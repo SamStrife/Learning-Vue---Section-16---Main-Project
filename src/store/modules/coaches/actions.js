a8ae4331414bcc046fe1f9b45a1e8c328db5c51c
Sam <sam.a.fletcher@gmail.com>
2022-01-23 11:59:51 +0000

    Added basic caching for coach list refreshing

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -26,7 +26,10 @@ export default {
       id: userId,
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
     const response = await fetch(
       `https://find-a-coach-app-87bc2-default-rtdb.firebaseio.com/coaches.json`
     );
@@ -50,5 +53,6 @@ export default {
       coaches.push(coach);
     }
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   },
 };
